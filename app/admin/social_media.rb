643624af6e1aa1d7c48fadfb4c27d721c5990d6e
rp-guruh <github.gurh@gmail.com>
2023-07-05 16:54:09 +0700

    create social media admin & erb

--- app/admin/social_media.rb
+++ app/admin/social_media.rb
@@ -1,19 +1,20 @@
 ActiveAdmin.register SocialMedium do
-
   permit_params :name, :icon, :url
-
   form do |f|
     f.inputs do
       f.input :name
       f.input :icon, as: :select, collection: [
         ["Facebook", "fa fa-facebook"],
         ["Instagram", "fa fa-instagram"],
-        ["Twitter", "fa fa-twitter"]
+        ["Twitter", "fa fa-twitter"],
+        ["Dribble", "fa fa-dribble"],
+        ["LinkedIn", "fa fa-linkedin"],
+        ["YouTube", "fa fa-youtube"],
+        ["WhatsApp", "fa fa-whatsapp"],
+        ["Telegram", "fa fa-telegram"]
       ]
       f.input :url
     end
     f.actions
   end
-
-  
 end
--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -2,6 +2,7 @@ class MainpageController < ApplicationController
     def index
         # get welcome, tagline text, and background image
         @tgline = HomePage::first
+        @socialMedia = SocialMedium::all
        
         render 'index'
     end
